bfb9e5a1f176cebd7940bf6561b02fe1fbc3eb22
Muhammad Habbibie Zikrillah <167674577+habibiezkrillh@users.noreply.github.com>
2024-10-03 10:42:49 +0800

    functons_arrays.php

--- Lab_exercise_2_[Muh Habbibie Zikrillah]/functons_arrays.php
+++ Lab_exercise_2_[Muh Habbibie Zikrillah]/functons_arrays.php
@@ -1,16 +1,11 @@
 <?php
-function greet($name, $day, $month, $year) {
-    return "Hello, " . $name . "! You were born on ". $day . "/" . $month . "/" . $year . ".";
+function greet($name) {
+    return "Hello, " . $name . "!";
 }
-$students = [
-    ["name" => "Habbibie", "day" => 30, "month" => 5, "year" => 2004],
-    ["name" => "Eva", "day" => 23, "month" => 8, "year" => 2002],
-    ["name" => "Fauzan", "day" => 17, "month" => 3, "year" => 2001],
-    ["name" => "Yanto", "day" => 9, "month" => 11, "year" => 1999],
-    ["name" => "Milano", "day" => 5, "month" => 6, "year" => 2002]
-];
 
-foreach($students as $student) {
-    echo greet($student["name"], $student["day"], $student["month"], $student["year"]). "<br>";
+$students = ["Adelyn", "Aditya", "Ryan", "Tino", "Calvino"];
+
+foreach ($students as $student) {
+    echo greet($student) . "<br>";
 }
 ?>
